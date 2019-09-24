d2c703c9379d06f0d0fe2995ef2979b441c21bde
wong9348 <55536737+wong9348@users.noreply.github.com>
2019-09-24 20:01:02 +0800

    Add files via upload

---
+++ lab1.cpp
@@ -0,0 +1,35 @@
+//WONG WEI KENG EXERCISE 1.3
+#include<iostream>
+using namespace std;
+int main ()
+{
+	cout<<"Q1."<<endl;
+	cout<<"University	: Universiti Teknikal Malaysia, Melaka"<<endl;
+	cout<<"Faculty		: FTMK"<<endl;
+	cout<<"Programme	: BITI"<<endl<<endl;
+	cout<<"My name is WONG WEI KENG. I am from Alor Setar"<<endl<<endl;
+	
+	
+	cout<<"Q2."<<endl;
+	cout<<"*************************************"<<endl;
+	cout<<"*****\t\tUTEM\t\t*****"<<endl;
+	cout<<"*************************************"<<endl<<endl;
+	
+	cout<<"Q3."<<endl;
+	cout<<"10"<<endl;
+	cout<<"9"<<endl;
+	cout<<"8"<<endl;
+	cout<<"7\t\t\t\t\t\t*"<<endl;
+	cout<<"6\t*"<<endl;
+	cout<<"5\t\t\t\t\t\t\t\t\t*"<<endl;
+	cout<<"4\t\t*"<<endl;
+	cout<<"3\t\t\t\t\t\t\t\t*"<<endl;
+	cout<<"2\t\t\t*"<<endl;
+	cout<<"1\t*"<<endl;
+	cout<<"0\t1\t2\t3\t4\t5\t6\t7\t8\t9\t10"<<endl;
+	
+	
+	
+return 0;
+}
+
